be5a178f7f1b4a8e502a4e5abcb8508e4a79f94a
virunpatch <guyver125@hotmail.com>
2020-10-25 00:08:55 +0700

    commit_3

--- commath/build/service-worker.js
+++ commath/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cmpj/precache-manifest.bcbc2f92cfb3cb3793b294d71a7b497a.js"
+  "/pro_commath/precache-manifest.bcbc2f92cfb3cb3793b294d71a7b497a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cmpj/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/[pro_commath]/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
